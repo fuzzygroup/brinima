c1678f8f36f11d77416d6f0da6795f4d4c01e1fd
Scott Johnson <fuzzygroup@gmail.com>
2022-12-18 07:25:58 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# The Brinima Theme
+
+This Bridgetown theme is a port of the Jekyll Minima theme to Bridgetown.
+
+## I'll Start with an Apology
+
+I have to admit that I'm a lousy, awful, terrible, no good front end person who, yet, 
+finds himself making a theme that is front end in nature.  Minima, when it works on Jekyll, 
+is lovely and any errors are really my fault.
+
+### If I'm Lousy at Front End Why Am I Doing a Theme?
+
+I'm doing this because Jekyll has been in life support mode for too damn long and Jared's 
+Bridgetown is the closest thing to a decent successor I've seen.  And I need minima for my own
+efforts so better to try and do something good than not.
+
+## And the Obligatory Self Promotion
+
+In case you are interested in the things that I do:
+
+* [Cartazzi](https://www.cartazzi.com) -- Cartazzi is my answer to what I call "Tab Hell".  Cartazzi allows any web developer to build an easy to use interface to all their tooling for a web development project
+* [FuzzyBlog](https://fuzzyblog.io/) -- the blog that used Minima which is 
---
+++ bridgetown_automation.rb
@@ -0,0 +1,119 @@
+# My thanks to Jared White's Bulmatown plugin from whom I stole most of this
+
+add_bridgetown_plugin "brinima"
+add_bridgetown_plugin "bridgetown-quick-search"
+add_bridgetown_plugin "bridgetown-feed"
+
+#add_yarn_for_gem "bulmatown"
+
+require 'fileutils'
+require 'shellwords'
+
+# unless Dir.exist? "frontend/fonts"
+#   FileUtils.mkdir_p "frontend/fonts"
+# end
+
+#run "cp node_modules/fork-awesome/fonts/* frontend/fonts"
+
+#javascript_import 'import Bulmatown from "bulmatown"'
+
+# *** Set up remote repo pull
+
+# Dynamically determined due to having to load from the tempdir
+@current_dir = File.expand_path(__dir__)
+
+# If its a remote file, the branch is appended to the end, so go up a level
+ROOT_PATH = if __FILE__ =~ %r{\Ahttps?://}
+              File.expand_path('../', __dir__)
+            else
+              File.expand_path(__dir__)
+            end
+
+DIR_NAME = File.basename(ROOT_PATH)
+
+# DIR_NAME = 'brinima'
+GITHUB_PATH = "https://github.com/fuzzygroup/#{DIR_NAME}.git"
+
+def backup_existing_theme_files
+  # generate a back up directory
+  create_directory("src/theme_backups")
+  # generate timestamp
+  backup_time_stamp = Time.now.utc.strftime("%Y%m%d%H%M%S")
+  create_directory("src/theme_backups/#{backup_time_stamp}")
+  # copy all files from directorys
+  source_directories = []
+  source_directories << "src/_layouts"
+  source_directories << "src/_components"
+  source_directories << "src/_layouts"
+
+  # copy files from sources
+end
+
+# Copied from: https://github.com/mattbrictson/rails-template
+# Add this template directory to source_paths so that Thor actions like
+# copy_file and template resolve against our source files. If this file was
+# invoked remotely via HTTP, that means the files are not present locally.
+# In that case, use `git clone` to download them to a local temporary dir.
+def add_template_repository_to_source_path
+  if __FILE__ =~ %r{\Ahttps?://}
+    require 'tmpdir'
+
+    source_paths.unshift(tempdir = Dir.mktmpdir(DIR_NAME + '-'))
+    at_exit { FileUtils.remove_entry(tempdir) }
+    run("git clone --quiet #{GITHUB_PATH.shellescape} #{tempdir.shellescape}")
+
+    if (branch = __FILE__[%r{#{DIR_NAME}/(.+)/bridgetown.automation.rb}, 1])
+      Dir.chdir(tempdir) { system("git checkout #{branch}") }
+      @current_dir = File.expand_path(tempdir)
+    end
+  else
+    source_paths.unshift(DIR_NAME)
+  end
+end
+
+def copy_if_exists(file)
+  target = File.exist?("src/_layouts/#{file}.html") ? "src/_layouts/#{file}.html" : "src/_layouts/#{file}.liquid"
+  copy_file "example/src/_layouts/#{file}.liquid", target
+end
+
+def substitute_in_default_if_exists
+  if File.exists?("src/_layouts/default.liquid")
+    gsub_file "src/_layouts/default.liquid", '{% render "footer", ', '{% render "footer", url: site.url, '
+  elsif File.exists?("src/_layouts/default.html")
+    gsub_file "src/_layouts/default.html", '{% render "footer", ', '{% render "footer", url: site.url, '
+  else
+    say_status :bulmatown, "Could not find the default template. You will have to add the url parameter to the render command manually"
+  end
+end
+
+if yes? "The Bulmatown installer can update styles, layouts, and page templates to use the new theme. You'll have the option to type 'a' to overwrite all existing files or 'd' to inspect each change. Would you like to proceed? (Y/N)"
+  add_template_repository_to_source_path
+
+  create_file "frontend/styles/index.scss", '@import "~bulmatown/frontend/styles"'
+
+  ["home", "page", "post"].each { |f| copy_if_exists(f) }
+  substitute_in_default_if_exists
+
+  copy_file "example/src/index.md", "src/index.md"
+  copy_file "example/src/posts.md", "src/posts.md"
+  copy_file "example/src/404.html", "src/404.html"
+  
+  copy_file "example/src/_components/navbar.liquid", "src/_components/navbar.liquid"
+  copy_file "example/src/_components/footer.liquid", "src/_components/footer.liquid"
+
+  inject_into_file "bridgetown.config.yml", "pagination:\n  enabled: true\n", after: "permalink: pretty\n"
+end
+
+twitter = ask "Do you have a Twitter handle? If so, enter it here, otherwise type 'no'"
+
+if twitter != "" && twitter != "no"
+  append_to_file "src/_data/site_metadata.yml" do
+    <<~YAML
+
+      twitter: #{twitter}
+
+    YAML
+  end
+end
+
+say_status :bulmatown, "Theme installation complete! Enjoy your fresh new design :)"
